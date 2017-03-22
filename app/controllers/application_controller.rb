75ff97fda1756c532f7abbad0edc5c49c41b22f3
David Celis <me@davidcel.is>
2017-03-21 17:02:54 -0700

    Add basic webhook route
    Signed-off-by: David Celis <me@davidcel.is>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+  before_action :verify_signature, only: [:hook]
+
+  def hook
+    event = request.headers['X-GitHub-Event']
+
+    Rails.logger.info "Received an event of type: #{event}"
+
+    send("handle_#{event}")
+  rescue LoadError
+    Rails.logger.info "We don't handle that type of event yet."
+  end
+
+  private
+
+  def verify_signature
+    webhook_secret = Rails.application.secrets.github_webhook_secret
+    digest = OpenSSL::Digest.new('sha1')
+    hexdigest = OpenSSL::HMAC.hexdigest(digest, webhook_secret, request.body)
+    signature = "sha1=#{hexdigest}"
+    hub_signature = request.headers['X-Hub-Signature']
+
+    unless Rack::Utils.secure_compare(signature, hub_signature)
+      render json: { error: "Signature did not match." }, status: 401
+    end
+  end
 end
